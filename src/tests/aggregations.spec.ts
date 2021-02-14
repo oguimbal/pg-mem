7c1ed406eae0f8c4a7ba7440d65e1346eacdff7d
maxvandongen <maxvandongen@yahoo.com>
2021-02-14 18:28:50 +0100

    Added SUM and SUM(DISTINCT) aggregators

--- src/tests/aggregations.spec.ts
+++ src/tests/aggregations.spec.ts
@@ -45,4 +45,30 @@ describe('Aggregations', () => {
                     select min(a) from example`))
             .to.deep.equal([{ min: null }]);
     })
+
+    it('supports sum()', () => {
+      expect(many(`create table example(a int);
+                  insert into example values (1), (-2), (null), (3), (-4), (5);
+                  select sum(a) from example`))
+      .to.deep.equal([{ sum: 3 }])
+    })
+
+    it('sum() returns null when nothing', () => {
+      expect(many(`create table example(a int);
+                  select sum(a) from example`))
+          .to.deep.equal([{ sum: null }]);
+  })
+
+  it('supports sum(distinct())', () => {
+    expect(many(`create table example(a int);
+                insert into example values (1), (1), (null), (2), (2), (3), (-1), (-1);
+                select sum(distinct(a)) from example`))
+    .to.deep.equal([{ sum: 5 }])
+  })
+
+  it('sum(distinct()) returns null when nothing', () => {
+    expect(many(`create table example(a int);
+                select sum(distinct(a)) from example`))
+        .to.deep.equal([{ sum: null }]);
+})
 });
