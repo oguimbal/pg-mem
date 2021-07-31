6182e62e7c786969ee1d2b39e2aac1d5e3a0fa48
Olivier Guimbal <olivier.guimbal@gadz.org>
2021-07-31 10:02:13 +0200

    Fix  #146

---
+++ src/tests/irl-tests/carlosfaria94.spec.ts
@@ -0,0 +1,59 @@
+import { newDb } from '../..';
+import * as Knex from 'knex';
+import { expect } from 'chai';
+import { knexSnakeCaseMappers } from 'objection';
+
+async function up(knex: Knex): Promise<void> {
+    return knex.schema
+        .createTable('update_times', function (table) {
+            table.string('id', 50).unique().notNullable().primary()
+            table.dateTime('update_time').notNullable()
+        })
+        .createTable('room_board_types', function (table) {
+            table.string('id', 2).unique().notNullable().primary()
+            table.string('name', 100).notNullable()
+            table.boolean('enabled').notNullable().defaultTo(false)
+            table.boolean('package').notNullable().defaultTo(false)
+        })
+        .createTable('room_types', function (table) {
+            table.string('id', 3).unique().notNullable().primary()
+            table.string('name', 100).notNullable()
+            table.boolean('enabled').notNullable().defaultTo(false)
+            table.boolean('package').notNullable().defaultTo(false)
+        })
+        .createTable('room_characteristics', function (table) {
+            table.string('id', 10).unique().notNullable().primary()
+            table.string('name', 100).nullable()
+            table.boolean('is_enabled').notNullable().defaultTo(true)
+        })
+        .createTable('room_types_characteristics', function (table) {
+            table.string('type_id', 3).notNullable().references('id').inTable('room_types')
+            table.string('characteristic_id', 10).notNullable().references('id').inTable('room_characteristics')
+            table.primary(['type_id', 'characteristic_id'])
+        })
+}
+
+
+describe('IRL tests', () => {
+    it('carlosfaria94 x Knex migration fails', async () => {
+        const mem = newDb();
+        const knex = mem.adapters.createKnex(0, {
+            // https://vincit.github.io/objection.js/api/objection/#knexsnakecasemappers
+            ...knexSnakeCaseMappers(),
+        }) as import('knex');
+        await up(knex);
+
+        mem.public.none(`insert into room_characteristics(id,name,is_enabled) values ('roomid', 'roomname', true)`);
+
+
+        const result = await knex('room_characteristics')
+            .select('id', 'is_enabled');
+
+        expect(result).to.deep.equal([{
+            id: 'roomid',
+            // snake cased
+            isEnabled: true,
+        }]);
+
+    });
+});
