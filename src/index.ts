d21f0616be3542834f175b52b3f147e50512b362
Misha Kaletsky <15040698+mmkal@users.noreply.github.com>
2024-03-05 17:31:47 -0800

    export replaceQueryArgs$ in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { newDb } from './db';
 export { enableStatementLocationTracking } from './parser/parse-cache';
+export { replaceQueryArgs$ } from './adapters';
 export * from './interfaces';
